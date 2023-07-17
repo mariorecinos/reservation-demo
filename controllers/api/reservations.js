c410734afc9b37c3a8d2c54861c59a9beea06864
mariorecinos <recinos.mario68@gmail.com>
2023-07-17 13:17:40 -0700

    Get All Reservation Controller

--- controllers/api/reservations.js
+++ controllers/api/reservations.js
@@ -2,11 +2,13 @@ const Reservation = require('../../models/reservation')
 
 module.exports = {
   createReservation,
-//   getAllReservations,
-//   getReservationById,
-//   updateReservation,
-//   deleteReservation
-// }
+  getAllReservations,
+  getReservationById,
+  updateReservation,
+  deleteReservation
+}
+
+// Controller for creating a new reservation
 
 async function createReservation(req, res) {
   try {
@@ -19,3 +21,14 @@ async function createReservation(req, res) {
     res.json(error)
   }
 }
+
+// Controller for retrieving all reservations
+asnyc function getAllReservations(req, res) {
+  try {
+    const reservations = await Reservation.find({ user: req.user._id });
+    res.json(reservations);
+  } catch (error) {
+    res.status(400).json({ error: 'Failed to get reservations' });
+  }
+};
+
